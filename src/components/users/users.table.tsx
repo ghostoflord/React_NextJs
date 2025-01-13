1ba3df59be9e527536fe9467738f6afb6169d35f
ghostoflord <ghost2892003zzz@gmail.com>
2025-01-13 09:42:33 +0700

    paginate update

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
 // import '../../styles/users.css';
-import { Table, Button, notification, Popconfirm, message } from 'antd';
+import { Table, Button, notification, Popconfirm } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { PlusOutlined } from '@ant-design/icons';
 import CreateUserModal from './create.user.model';
 import UpdateUserModal from './update.user.model';
-
 
 export interface IUsers {
     _id: string;
@@ -28,8 +27,16 @@ const UsersTable = () => {
 
     const [dataUpdate, setDataUpdate] = useState<null | IUsers>(null);
 
-    const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjc3ZThlODljOGY3N2U4YTM4NDA3OTM3IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MzY2NTQxNjksImV4cCI6MTgyMzA1NDE2OX0.BbZXIXrqVncWH8FgQVJtn8sdP7PjHRhS1IQhUYLMNJw"
 
+    const access_token = localStorage.getItem("access_token") as string;
+
+
+    const [meta, setMeta] = useState({
+        current: 1,
+        pageSize: 5,
+        pages: 0,
+        total: 0
+    })
 
     useEffect(() => {
         //update
@@ -40,7 +47,7 @@ const UsersTable = () => {
     const getData = async () => {
 
         const res = await fetch(
-            "http://localhost:8000/api/v1/users/all",
+            `http://localhost:8000/api/v1/users?current=${meta.current}&pageSize=${meta.pageSize}`,
             {
                 headers: {
                     'Authorization': `Bearer ${access_token}`,
@@ -55,6 +62,12 @@ const UsersTable = () => {
             })
         }
         setListUsers(d.data.result)
+        setMeta({
+            current: d.data.meta.current,
+            pageSize: d.data.meta.pageSize,
+            pages: d.data.meta.pages,
+            total: d.data.meta.total
+        })
     }
 
     const confirm = async (user: IUsers) => {
@@ -127,6 +140,30 @@ const UsersTable = () => {
     ]
 
 
+    const handleOnChange = async (page: number, pageSize: number) => {
+        const res = await fetch(
+            `http://localhost:8000/api/v1/users?current=${page}&pageSize=${pageSize}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    "Content-Type": "application/json",
+                },
+            })
+
+        const d = await res.json();
+        if (!d.data) {
+            notification.error({
+                message: JSON.stringify(d.message)
+            })
+        }
+        setListUsers(d.data.result)
+        setMeta({
+            current: d.data.meta.current,
+            pageSize: d.data.meta.pageSize,
+            pages: d.data.meta.pages,
+            total: d.data.meta.total
+        })
+    }
 
     return (
         <div>
@@ -150,6 +187,14 @@ const UsersTable = () => {
                 columns={columns}
                 dataSource={listUsers}
                 rowKey={"_id"}
+                pagination={{
+                    current: meta.current,
+                    pageSize: meta.pageSize,
+                    total: meta.total,
+                    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+                    onChange: (page: number, pageSize: number) => handleOnChange(page, pageSize),
+                    showSizeChanger: true
+                }}
             />
 
             <CreateUserModal
